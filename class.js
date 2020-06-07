4ebe3ecf5091e3cadf8f988c67f7db11626ea193
James Oswald <jamesoswald111@gmail.com>
2020-06-07 01:49:13 -0400

    Add files via upload

---
+++ class.js
@@ -0,0 +1,31 @@
+
+function getUrlParam(param){
+    let params = {};
+    let parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m,key,value){
+        params[key] = value;
+    });
+    return params[param];
+}
+
+function propagate(){
+	let name = getUrlParam("c");
+	fetch("./Classes/" + name + ".json", {credentials: 'same-origin'})
+	.then(r => r.json())
+	.then(d => propcont(d));
+}
+
+let globdata;
+function propcont(data){
+	globdata = data;
+	document.getElementById("className").innerHTML = data["className"];
+	document.getElementById("rightimg").src = data["rightimg"];
+	document.getElementById("leftimg").src = data["leftimg"];
+	document.getElementById("leftimg").src = data["leftimg"];
+	document.getElementById("classDescription").innerHTML = data["classDescription"];
+	let propString = "<ul>";
+	for(let i = 0; i < data["classProperties"].length; i++){
+		propString += "<li>" + data["classProperties"][i];
+	}
+	propString += "</ul>"
+	document.getElementById("classProp").innerHTML = propString;
+}
---
+++ sim.js
@@ -0,0 +1,69 @@
+
+
+let waifus = null;
+let rolled = null;
+let rarityClasses = [[], [], [], [], []];
+let obtained = [];
+
+function onFetch(data){
+	waifus = data;
+	for(let i = 0; i < waifus.length; i++)
+		rarityClasses[waifus[i]["rarity"] - 1].push(waifus[i]);
+}
+
+fetch("fgo.json").then(r => r.json()).then(d => onFetch(d));
+
+function roll(){
+	if(waifus == null){
+		alert("Please Wait a moment before rolling waifus.")
+		return;
+	}
+	let list = null;
+	let roll = Math.floor(Math.random() * 100) + 1;
+	if(roll == 1)
+		list = rarityClasses[4];
+	else if(roll >= 2 && roll <= 5)
+		list = rarityClasses[3];
+	else if(roll >= 6 && roll <= 25)
+		list = rarityClasses[2];
+	else if(roll >= 26 && roll <= 55)
+		list = rarityClasses[1];
+	else
+		list = rarityClasses[0];
+	rolled = list[Math.floor(Math.random() * list.length)];
+	obtained.push(rolled);
+	updateDisplay();
+}
+
+function clearRolls(){
+	rolled = null;
+	obtained = [];
+	obHTML = "";
+	updateDisplay();
+}
+
+let obHTML = "";
+function updateDisplay(){
+	if(rolled != null){
+		//let stars = "★";
+		let stars = "★".repeat(rolled["rarity"]);
+		//stars = stars.repeat(rolled["rarity"]);
+		document.getElementById("lRoll").innerHTML = stars + " " + rolled["name"];
+		document.getElementById("lRollImg").src = rolled["img1"];
+		obHTML += "<div class=\"il r" + rolled["rarity"] + "\">";
+		obHTML += "<p class=\"obtRoll\">" + stars + " " + rolled["name"] + "</p>";
+		obHTML += "<img class=\"obtRollImg\" alt=\"\" src=\"" + rolled["img1"] + "\"/>"
+		obHTML += "</div>";
+	}else{
+		document.getElementById("lRoll").innerHTML = "Click Roll to roll";
+		document.getElementById("lRollImg").src = "meme.gif";
+	}
+	/*for(let i = 0; i < obtained.length; i++){
+		obHTML += "<div class=\"il r" + obtained[i]["rarity"] + "\">";
+		let stars = "★".repeat(obtained[i]["rarity"]);
+		obHTML += "<p class=\"obtRoll\">" + stars + " " + obtained[i]["name"] + "</p>";
+		obHTML += "<img class=\"obtRollImg\" alt=\"\" src=\"" + obtained[i]["img1"] + "\"/>"
+		obHTML += "</div>";
+	}*/
+	document.getElementById("obt").innerHTML = obHTML;
+}
